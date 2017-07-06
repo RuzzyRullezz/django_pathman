cf2815c4d589f2dbfdff228ce2deff8f2eb2d163
rgilfanov <rgilfanov@fix.ru>
2017-07-06 18:57:51 +0300

    sql data fix

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     version=django_pathman.__version__,
     packages=find_packages(),
     # long_description=open(join(dirname(__file__), 'README.txt')).read(),
+    package_data={'sql': ['sql/*.sql', 'sql/README']},
     install_requires=[
         'django>=1.9.5',
     ],
