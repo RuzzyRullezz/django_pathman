c85af7a771379e02b3838accb0bc9c13cabf3267
rgilfanov <rgilfanov@fix.ru>
2017-07-12 12:43:04 +0300

    index naming fix

--- django_pathman/sql/alter_child_index_v2.sql
+++ django_pathman/sql/alter_child_index_v2.sql
@@ -41,7 +41,7 @@ BEGIN
             IF (new_idx_name = diff_idx.indexname) THEN
                 new_idx_name := new_idx_name || '_' || substring(md5(random()::text) from 1 for 10);
                 new_idx_name := substring(new_idx_name from 1 for max_index_name_length);
-            ENDIF;
+            END IF;
             new_idx_def := regexp_replace(diff_idx.indexdef, 'CREATE (UNIQUE |)INDEX (' || diff_idx.indexname || ') ON (' || parent_table || ') ', 'CREATE \1INDEX ' || new_idx_name || ' ON ' || itable || ' ');
             IF(is_debug) THEN
                 RAISE INFO 'Creating index "%" ON "%.%"...', new_idx_name, schema_name, itable;
